9c93b0d2a6550e963720faf12ff48b30f7b8e8e3
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-07-29 14:28:50 -0400

    spotify id updated

--- liri.js
+++ liri.js
@@ -2,16 +2,35 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var axios = require("axios");
-
+var command = process.argv[2];
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-function concert_this(artist){
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    
-    axios.get(queryURL).then(
-        function(response){
-            console.log(response);
-        }
-    )
+
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
+
+function concertThis(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + "khalid" + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(function(response) {
+        console.log(response.data);
+    })
 }
-concert_this(response.venue);
+
+
+function spotifyThisSong(song) {
+    var artist = process.argv[3];
+}
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
